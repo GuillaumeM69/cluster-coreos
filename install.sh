eb58d4833f4be55c3835e824347ae8109c3a0645
Guillaume M <guillaume.Melon@gmail.com>
2017-02-01 01:31:12 +0100

    https://goo.gl/VP88QB

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+#https://goo.gl/VP88QB
 export myip=`ifconfig | awk '{ print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | awk NR==1`
-wget  -O cloud-config.yaml http://goo.gl/l7br8P
+wget  -O cloud-config.yaml https://raw.githubusercontent.com/GuillaumeM69/cluster-coreos/master/cloud-config.yaml
 sed -i 's|$private_ipv4|'$myip'|g' cloud-config.yaml
 coreos-install -d /dev/sda -C stable -c cloud-config.yaml
